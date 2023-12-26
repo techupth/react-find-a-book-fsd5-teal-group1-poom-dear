0c235211c862cc4e6be4af16dbdedb16acab934f
Kittipoom Puttaruksa <kittipoom@Kittipooms-MacBook-Pro.local>
2023-12-26 12:22:08 +0700

    finish ex 2,4

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
+import { useEffect, useState } from "react";
+import { DebounceInput } from "react-debounce-input";
 import "./App.css";
-
+import axios from "axios";
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const [book, setBook] = useState([]);
+  const [inputSearch, setInputSearch] = useState("");
+  const [status, setStatus] = useState("loading...");
+  async function search() {
+    try {
+      const searchItem = await axios.get(
+        `https://www.googleapis.com/books/v1/volumes?q=${inputSearch}`
+      );
+      setBook(searchItem.data.items);
+      console.log(searchItem.data.items);
+    } catch (error) {
+      setStatus(error);
+    }
+  }
+  useEffect(() => {
+    inputSearch === "" ? setBook([]) : search();
+  }, [inputSearch]);
+
+  return (
+    <div className="App">
+      <h1>Find a Book</h1>
+      <DebounceInput
+        type="text"
+        minLength={2}
+        debounceTimeout={500}
+        value={inputSearch}
+        onChange={(e) => {
+          setInputSearch(e.target.value);
+        }}
+      />
+      {book.map((item, index) => {
+        return <li key={index}> {item.volumeInfo.title}</li>;
+      })}
+    </div>
+  );
 }
 
 export default App;
